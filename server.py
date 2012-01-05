1e4c5051b22e03dece90cdecac80ea063fbf266c
a_japonicus <arctoscopus.japonicus@gmail.com>
2012-01-06 02:54:09 +0900

    server port 8000 -> 80

--- server.py
+++ server.py
@@ -10,9 +10,12 @@
 # Copyright:   (c) hatahata 2012
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import BaseHTTPServer
 import CGIHTTPServer
 
 # TODO:全てのディレクトリでCGIが動く簡易サーバの作成
 # 現在はpublic_html内のみ。サブディレクトリを見てくれない・・・
+#CGIHTTPServer.CGIHTTPRequestHandler.cgi_directories = ['/public_html']
+#CGIHTTPServer.test()
 CGIHTTPServer.CGIHTTPRequestHandler.cgi_directories = ['/public_html']
-CGIHTTPServer.test()
+BaseHTTPServer.HTTPServer(('', 80), CGIHTTPServer.CGIHTTPRequestHandler).serve_forever()
